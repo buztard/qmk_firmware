6979b1fc0ad376570379389fff8dd919bdaffb01
Bastian Winkler <buz@netbuz.org>
2019-04-23 01:02:42 +0200

    crkbd: Add common OLED support

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -65,10 +65,14 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-
+SUBPROJECT_rev1 = no
+USE_I2C = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 CUSTOM_MATRIX = yes
 
 DEFAULT_FOLDER = crkbd/rev1
+
+# Custom local font file
+OPT_DEFS += -DOLED_FONT_H=\"lib/glcdfont.c\"
