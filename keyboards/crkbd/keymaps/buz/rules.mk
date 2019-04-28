4a691f574165981a182c786fd5726a1160d3e12d
Bastian Winkler <buz@netbuz.org>
2019-04-28 15:40:51 +0200

    crkbd: Toying around

--- keyboards/crkbd/keymaps/buz/rules.mk
+++ keyboards/crkbd/keymaps/buz/rules.mk
@@ -4,7 +4,7 @@
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = no        # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -22,4 +22,4 @@ OLED_DRIVER_ENABLE = yes    # OLED_ENABLE (+5000)
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-RAW_ENABLE = yes
+RAW_ENABLE = no
