7eefdd1205bbea284e07388375f31f8aa83bbacf
Bastian Winkler <buz@netbuz.org>
2019-04-29 16:03:07 +0200

    userspace: Initial buz userspace

---
+++ users/buz/rules.mk
@@ -0,0 +1,21 @@
+SRC += buz.c
+
+ifeq ($(PLATFORM), AVR)
+  EXTRAFLAGS += -flto
+endif
+
+ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
+  SRC += tap_dances.c
+endif
+
+ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
+  SRC += rgb_stuff.c
+endif
+
+ifneq ($(strip $(RGB_MATRIX_ENABLE)), no)
+  SRC += rgb_stuff.c
+endif
+
+ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
+  SRC += oled_stuff.c
+endif
