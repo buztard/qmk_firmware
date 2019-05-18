bb0414e495313ba0c885c1b6f52c30890419017c
Bastian Winkler <buz@netbuz.org>
2019-05-19 01:23:44 +0200

    planck: Disable RGBLIGHT

--- keyboards/planck/keymaps/buz/rules.mk
+++ keyboards/planck/keymaps/buz/rules.mk
@@ -1,5 +1,5 @@
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 AUDIO_ENABLE = yes          # Audio output on port C6
 
 SRC += muse.c
