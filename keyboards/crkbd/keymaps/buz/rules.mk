943066225f28ba9d697a8595028f6568d4a51cef
Bastian Winkler <buz@netbuz.org>
2019-09-10 20:33:13 +0200

    crkbd: Use dfu bootloader by default

--- keyboards/crkbd/keymaps/buz/rules.mk
+++ keyboards/crkbd/keymaps/buz/rules.mk
@@ -24,3 +24,5 @@ LEADER_ENABLE = no
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 RAW_ENABLE = no
+
+BOOTLOADER = atmel-dfu
