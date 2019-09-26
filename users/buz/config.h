05bedb5c1bc7f24137306af945121123fa4a5e69
Bastian Winkler <buz@netbuz.org>
2019-09-26 17:59:36 +0200

    userspace: Some clang-format

--- users/buz/config.h
+++ users/buz/config.h
@@ -2,12 +2,13 @@
 
 #define TAPPING_FORCE_HOLD
 #ifdef TAPPING_TERM
-#undef TAPPING_TERM
-#define TAPPING_TERM 200
+#    undef TAPPING_TERM
+#    define TAPPING_TERM 200
 #endif
+
 #define LEADER_PER_KEY_TIMING
 #define LEADER_TIMEOUT 250
 
 #ifndef LEADER_ENABLE
-#define KC_LEAD KC_TRANSPARENT
+#    define KC_LEAD KC_TRANSPARENT
 #endif
