bd5dfccafd4ab74ebc67cdb0a853cf969838b423
Bastian Winkler <buz@netbuz.org>
2019-10-01 01:06:13 +0200

    userspace: Tap dance improvements

--- users/buz/tap_dances.c
+++ users/buz/tap_dances.c
@@ -2,15 +2,18 @@
 #include "tap_dances.h"
 #include "buz.h"
 
-td_state_t tap_lalt = TAP_END;
-td_state_t tap_ralt = TAP_END;
+static td_state_t tap_lalt = TAP_END;
+static td_state_t tap_ralt = TAP_END;
+static td_state_t tap_f    = TAP_END;
 
-int cur_dance(qk_tap_dance_state_t *state) {
+static int cur_dance(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
-        if (state->interrupted || !state->pressed) {
-            return SINGLE_TAP;
-        } else {
+        if (state->interrupted) {
+            return SINGLE_SHORT_TAP;
+        } else if (state->pressed) {
             return SINGLE_HOLD;
+        } else {
+            return SINGLE_TAP;
         }
     } else if (state->count == 2) {
         if (state->interrupted) {
@@ -21,17 +24,19 @@ int cur_dance(qk_tap_dance_state_t *state) {
             return DOUBLE_TAP;
         }
     } else if (state->count == 3) {
-        if (state->interrupted || !state->pressed) {
-            return TRIPLE_TAP;
-        } else {
+        if (state->interrupted) {
+            return TRIPLE_SINGLE_TAP;
+        } else if (state->pressed) {
             return TRIPLE_HOLD;
+        } else {
+            return TRIPLE_TAP;
         }
     } else {
         return TAP_END;
     }
 }
 
-void td_lalt_finished(qk_tap_dance_state_t *state, void *user_data) {
+static void td_lalt_finished(qk_tap_dance_state_t *state, void *user_data) {
     tap_lalt = cur_dance(state);
     switch (tap_lalt) {
         case SINGLE_TAP:
@@ -41,7 +46,7 @@ void td_lalt_finished(qk_tap_dance_state_t *state, void *user_data) {
             register_mods(MOD_BIT(KC_LALT));
             break;
         case DOUBLE_TAP:
-            if (biton32(layer_state) == _MOUSE) {
+            if (IS_LAYER_ON(_MOUSE)) {
                 layer_off(_MOUSE);
             } else {
                 layer_on(_MOUSE);
@@ -54,7 +59,7 @@ void td_lalt_finished(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
-void td_lalt_reset(qk_tap_dance_state_t *state, void *user_data) {
+static void td_lalt_reset(qk_tap_dance_state_t *state, void *user_data) {
     switch (tap_lalt) {
         case SINGLE_TAP:
             unregister_mods(MOD_BIT(KC_LALT));
@@ -71,17 +76,17 @@ void td_lalt_reset(qk_tap_dance_state_t *state, void *user_data) {
     tap_lalt = TAP_END;
 }
 
-void td_ralt_finished(qk_tap_dance_state_t *state, void *user_data) {
+static void td_ralt_finished(qk_tap_dance_state_t *state, void *user_data) {
     tap_ralt = cur_dance(state);
     switch (tap_ralt) {
+        case SINGLE_SHORT_TAP:
         case SINGLE_TAP:
-            register_mods(MOD_BIT(KC_RALT));
-            break;
+            set_oneshot_mods(MOD_BIT(KC_RALT));
         case SINGLE_HOLD:
             register_mods(MOD_BIT(KC_RALT));
             break;
         case DOUBLE_TAP:
-            if (biton32(layer_state) == _VIM) {
+            if (IS_LAYER_ON(_VIM)) {
                 layer_off(_VIM);
             } else {
                 layer_on(_VIM);
@@ -95,11 +100,10 @@ void td_ralt_finished(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
-void td_ralt_reset(qk_tap_dance_state_t *state, void *user_data) {
+static void td_ralt_reset(qk_tap_dance_state_t *state, void *user_data) {
     switch (tap_ralt) {
         case SINGLE_TAP:
-            unregister_mods(MOD_BIT(KC_RALT));
-            break;
+        case SINGLE_SHORT_TAP:
         case SINGLE_HOLD:
             unregister_mods(MOD_BIT(KC_RALT));
             break;
@@ -114,7 +118,52 @@ void td_ralt_reset(qk_tap_dance_state_t *state, void *user_data) {
     tap_ralt = TAP_END;
 }
 
+static void td_f_finished(qk_tap_dance_state_t *state, void *user_data) {
+    tap_f = cur_dance(state);
+
+    switch (tap_f) {
+        case SINGLE_SHORT_TAP:
+        case SINGLE_TAP:
+            register_code(KC_F);
+            break;
+        case SINGLE_HOLD:
+            layer_on(_VIM);
+            break;
+        case DOUBLE_SINGLE_TAP:
+        case DOUBLE_TAP:
+            register_code(KC_F);
+            unregister_code(KC_F);
+            register_code(KC_F);
+            break;
+        case DOUBLE_HOLD:
+            layer_on(_MOUSE);
+            break;
+        default:
+            break;
+    }
+}
+
+static void td_f_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (tap_f) {
+        case SINGLE_SHORT_TAP:
+        case SINGLE_TAP:
+        case DOUBLE_SINGLE_TAP:
+        case DOUBLE_TAP:
+            unregister_code(KC_F);
+            break;
+        case SINGLE_HOLD:
+            layer_off(_VIM);
+            break;
+        case DOUBLE_HOLD:
+            layer_off(_MOUSE);
+            break;
+        default:
+            break;
+    }
+}
+
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_LALT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, td_lalt_finished, td_lalt_reset, 300),
-    [TD_RALT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, td_ralt_finished, td_ralt_reset, 300),
+    [TD_LALT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, td_lalt_finished, td_lalt_reset, 200),
+    [TD_RALT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, td_ralt_finished, td_ralt_reset, 200),
+    [TD_F]    = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, td_f_finished, td_f_reset, 175),
 };
