48bd47a1965e9d95c515d23ac9130839b97684ac
Bastian Winkler <buz@netbuz.org>
2019-10-01 01:06:13 +0200

    userspace: Tap dance improvements

--- users/buz/tap_dances.h
+++ users/buz/tap_dances.h
@@ -3,17 +3,20 @@
 // tap dance keycodes
 enum {
     TD_LALT = 0,
-    TD_RALT
+    TD_RALT,
+    TD_F
 };
 
 typedef enum {
     SINGLE_TAP,
+    SINGLE_SHORT_TAP,
     SINGLE_HOLD,
     DOUBLE_TAP,
     DOUBLE_HOLD,
     DOUBLE_SINGLE_TAP,
     TRIPLE_TAP,
     TRIPLE_HOLD,
+    TRIPLE_SINGLE_TAP,
     TAP_END
 } td_state_t;
 
