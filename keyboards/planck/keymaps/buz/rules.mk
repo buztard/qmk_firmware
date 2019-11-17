600a02487a1c33ce28d42a5702bcf1b30709f9ed
Bastian Winkler <buz@netbuz.org>
2019-11-17 22:17:35 +0100

    planck: Update keymap

--- keyboards/planck/keymaps/buz/rules.mk
+++ keyboards/planck/keymaps/buz/rules.mk
@@ -1,5 +1,7 @@
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
-AUDIO_ENABLE = yes          # Audio output on port C6
+MOUSEKEY_ENABLE  = yes # Mouse keys(+4700)
+RGBLIGHT_ENABLE  = yes # Enable WS2812 RGB underlight.
+AUDIO_ENABLE     = yes # Audio output on port C6
+TAP_DANCE_ENABLE = yes
+LEADER_ENABLE    = yes
 
 SRC += muse.c
