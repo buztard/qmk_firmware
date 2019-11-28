28f42fe3523b7cef0df3d1e1000903dc485b926f
Bastian Winkler <buz@netbuz.org>
2019-11-28 09:48:45 +0100

    userspace: Add initial (not properly working) raise td

--- users/buz/tap_dances.h
+++ users/buz/tap_dances.h
@@ -4,19 +4,19 @@
 enum {
     TD_LALT = 0,
     TD_RALT,
-    TD_F
+    TD_F,
+    TD_RAISE,
 };
 
 typedef enum {
-    SINGLE_TAP,
-    SINGLE_SHORT_TAP,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP,
-    TRIPLE_TAP,
-    TRIPLE_HOLD,
-    TRIPLE_SINGLE_TAP,
-    TAP_END
+    SINGLE_TAP,         // a single tap
+    SINGLE_SHORT_TAP,   // single key press, interrupted by pressing another key
+    SINGLE_HOLD,        // hold a single key until timeout
+    DOUBLE_TAP,         // double tap
+    DOUBLE_HOLD,        // tap once, then hold
+    DOUBLE_SINGLE_TAP,  // two key presses, interrupted by pressing another key
+    TRIPLE_TAP,         // triple tap
+    TRIPLE_HOLD,        // two taps, then hold
+    TRIPLE_SINGLE_TAP,  // three taps, interrupted by pressing another key
+    TAP_END             // enough for now, close with a magic number...
 } td_state_t;
-
