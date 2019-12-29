37dbdfe978fe08f013c110faa87ff67a0d867931
Bastian Winkler <buz@netbuz.org>
2019-12-29 19:31:27 +0100

    buz: Disable some rgb matix modes by default

--- users/buz/config.h
+++ users/buz/config.h
@@ -16,3 +16,26 @@
 #ifndef LEADER_ENABLE
 #    define KC_LEAD KC_TRANSPARENT
 #endif
+
+#ifdef RGB_MATRIX_ENABLE
+// disable these modes as there are mult-keypress versions as well
+#    ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#    endif
+
+#    ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#    endif
+
+#    ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#    endif
+
+#    ifdef DISABLE_RGB_MATRIX_SPLASH
+#        define DISABLE_RGB_MATRIX_SPLASH
+#    endif
+
+#    ifdef DISABLE_RGB_MATRIX_SOLID_SPLASH
+#        define DISABLE_RGB_MATRIX_SOLID_SPLASH
+#    endif
+#endif
