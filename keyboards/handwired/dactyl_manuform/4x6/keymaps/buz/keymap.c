db986f6de2fa70eef37b0319cbcbc98cb24f31d3
Bastian Winkler <buz@netbuz.org>
2020-04-18 19:59:54 +0200

    buz: Migrate Vim layer to V

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/buz/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/buz/keymap.c
@@ -4,9 +4,9 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_wrapper(
-    KC_TABMS,_________________QWERTY_L1_________________,  _________________QWERTY_R1_________________, KC_BSPC,
+   KC_TABMS, _________________QWERTY_L1_________________,  _________________QWERTY_R1_________________, KC_BSPC,
     KC_CESC, _________________QWERTY_L2_________________,  _________________QWERTY_R2_________________, RCTL_T(KC_QUOT),
-    KC_LSPO, _________________QWERTY_L3_________________,  _________________QWERTY_R3_________________, KC_RSPC,
+       LSPO, _________________QWERTY_L3_________________,  _________________QWERTY_R3_________________, RSPC,
                            KC_LCBR, KC_RCBR,                            KC_LBRC, KC_RBRC,
                                     LOWER,   LGUI_T(KC_ENT),    KC_SPC,  RAISE,
                                  KC_TD_LALT, KC_LSPO,           KC_RSPC, KC_TD_RALT,
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_COLEMAK] = LAYOUT_wrapper(
-    KC_TABMS,_________________COLEMAK_L1________________,  _________________COLEMAK_R1________________, KC_BSPC,
+   KC_TABMS, _________________COLEMAK_L1________________,  _________________COLEMAK_R1________________, KC_BSPC,
     KC_CESC, _________________COLEMAK_L2________________,  _________________COLEMAK_R2________________, RCTL_T(KC_QUOT),
-    KC_LSPO, _________________COLEMAK_L3________________,  _________________COLEMAK_R3________________, KC_RSPC,
+       LSPO, _________________COLEMAK_L3________________,  _________________COLEMAK_R3________________, RSPC,
                            KC_LCBR, KC_RCBR,                            KC_LBRC, KC_RBRC,
                                     LOWER,   KC_GENT,           KC_SPC,  RAISE,
                                  KC_TD_LALT, KC_LSPO,           KC_RSPC, KC_TD_RALT,
