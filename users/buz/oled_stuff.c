ccaa0d4a82ca40779447b284e8cb427d392da6e9
Bastian Winkler <buz@netbuz.org>
2020-08-30 09:56:31 +0200

    OLED improvements

--- users/buz/oled_stuff.c
+++ users/buz/oled_stuff.c
@@ -8,33 +8,35 @@ extern bool leading;
 const char* get_layer_name(void) {
     switch (get_highest_layer(layer_state)) {
         case _LOWER:
-            return PSTR("Lower");
+            return PSTR(OLED_TEXT_LAYER_LOWER);
         case _RAISE:
-            return PSTR("Raise");
+            return PSTR(OLED_TEXT_LAYER_RAISE);
         case _ADJUST:
-            return PSTR("Adjust");
+            return PSTR(OLED_TEXT_LAYER_ADJUST);
         case _VIM:
-            return PSTR("Vim");
+            return PSTR(OLED_TEXT_LAYER_VIM);
         case _MOUSE:
-            return PSTR("Mouse");
+            return PSTR(OLED_TEXT_LAYER_MOUSE);
         case _NUM:
-            return PSTR("Numpad");
+            return PSTR(OLED_TEXT_LAYER_NUM);
         case _FN:
-            return PSTR("FN");
+            return PSTR(OLED_TEXT_LAYER_FN);
+        case _MEDIA:
+            return PSTR(OLED_TEXT_LAYER_MEDIA);
         default:
             switch (get_highest_layer(default_layer_state)) {
                 case _QWERTY:
-                    return PSTR("QUERTY");
+                    return PSTR(OLED_TEXT_LAYOUT_QUERTY);
                 case _COLEMAK:
-                    return PSTR("COLEMAK");
+                    return PSTR(OLED_TEXT_LAYOUT_COLEMAK);
                 default:
-                    return PSTR("Unknown");
+                    return PSTR(OLED_TEXT_UNKNOWN);
             }
     }
 }
 
 void oled_render_layer(void) {
-    oled_write_P(PSTR("L: "), false);
+    oled_write_P(PSTR(OLED_TEXT_LAYOUT_NAME), false);
     oled_write_P(get_layer_name(), false);
     oled_write_P(PSTR("\n"), false);
 
@@ -51,30 +53,48 @@ void oled_render_mods(void) {
     uint8_t led_usb_state = host_keyboard_leds();
     bool    mod           = false;
 
-    oled_write_P(PSTR("M: "), false);
+    oled_write_P(PSTR(OLED_TEXT_MOD_NAME), false);
     if ((modifiers & MOD_MASK_CTRL) || (one_shot & MOD_MASK_CTRL)) {
-        oled_write_P(PSTR("CTRL "), false);
+        oled_write_P(PSTR(OLED_TEXT_MOD_CTRL " "), false);
         mod = true;
     }
     if ((modifiers & MOD_MASK_GUI) || (one_shot & MOD_MASK_GUI)) {
-        oled_write_P(PSTR("GUI "), false);
+        oled_write_P(PSTR(OLED_TEXT_MOD_GUI " "), false);
         mod = true;
     }
     if ((modifiers & MOD_MASK_ALT) || (one_shot & MOD_MASK_ALT)) {
-        oled_write_P(PSTR("ALT "), false);
+        oled_write_P(PSTR(OLED_TEXT_MOD_ALT " "), false);
         mod = true;
     }
     if ((modifiers & MOD_MASK_SHIFT) || (one_shot & MOD_MASK_SHIFT)) {
-        oled_write_P(PSTR("SFT "), false);
+        oled_write_P(PSTR(OLED_TEXT_MOD_SHIFT " "), false);
         mod = true;
     }
     if (led_usb_state & (1 << USB_LED_CAPS_LOCK)) {
-        oled_write_P(PSTR("CAP "), false);
+        oled_write_P(PSTR(OLED_TEXT_MOD_CAPS " "), false);
         mod = true;
     }
     if (!mod) {
-        oled_write_P(PSTR("None"), false);
+        oled_write_P(PSTR(OLED_TEXT_NONE), false);
     }
+    oled_write_P(PSTR("\n"), false);
+}
+
+void oled_render_mods_XXX(void) {
+    uint8_t modifiers     = get_mods();
+    uint8_t one_shot      = get_oneshot_mods();
+    uint8_t led_usb_state = host_keyboard_leds();
+
+    oled_write_P(PSTR(OLED_TEXT_MOD_NAME), false);
+    oled_write_P(PSTR("C"), ((modifiers & MOD_MASK_CTRL) || (one_shot & MOD_MASK_CTRL)));
+    // oled_write_P(PSTR(" "), false);
+    oled_write_P(PSTR("G"), ((modifiers & MOD_MASK_GUI) || (one_shot & MOD_MASK_GUI)));
+    // oled_write_P(PSTR(" "), false);
+    oled_write_P(PSTR("A"), ((modifiers & MOD_MASK_ALT) || (one_shot & MOD_MASK_ALT)));
+    // oled_write_P(PSTR(" "), false);
+    oled_write_P(PSTR("S"), ((modifiers & MOD_MASK_SHIFT) || (one_shot & MOD_MASK_SHIFT)));
+    // oled_write_P(PSTR(" "), false);
+    oled_write_P(PSTR("C"), (led_usb_state & (1 << USB_LED_CAPS_LOCK)));
     oled_write_P(PSTR("\n"), false);
 }
 
@@ -82,56 +102,57 @@ void oled_render_mods(void) {
 void oled_render_rgblight_effect_name(void) {
     switch (rgblight_get_mode()) {
         case RGBLIGHT_MODE_STATIC_LIGHT:
-            oled_write_P(PSTR("Static\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_STATIC_LIGHT), false);
             break;
 #        ifdef RGBLIGHT_EFFECT_BREATHING
         case RGBLIGHT_MODE_BREATHING:
-            oled_write_P(PSTR("Breathing\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_BREATHING), false);
             break;
 #        endif
 #        ifdef RGBLIGHT_EFFECT_RAINBOW_MOOD
         case RGBLIGHT_MODE_RAINBOW_MOOD:
-            oled_write_P(PSTR("Rainbow mood\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_RAINBOW_MOOD), false);
             break;
 #        endif
 #        ifdef RGBLIGHT_EFFECT_RAINBOW_SWIRL
         case RGBLIGHT_MODE_RAINBOW_SWIRL:
-            oled_write_P(PSTR("Rainbow swirl\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_RAINBOW_SWIRL), false);
             break;
 #        endif
 #        ifdef RGBLIGHT_EFFECT_SNAKE
         case RGBLIGHT_MODE_SNAKE:
-            oled_write_P(PSTR("Snake\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_SNAKE), false);
             break;
 #        endif
 #        ifdef RGBLIGHT_EFFECT_KNIGHT
         case RGBLIGHT_MODE_KNIGHT:
-            oled_write_P(PSTR("Knight\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_KNIGHT), false);
             break;
 #        endif
 #        ifdef RGBLIGHT_EFFECT_CHRISTMAS
         case RGBLIGHT_MODE_CHRISTMAS:
-            oled_write_P(PSTR("Christmas\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_CHRISTMAS), false);
             break;
 #        endif
 #        ifdef RGBLIGHT_EFFECT_STATIC_GRADIENT
         case RGBLIGHT_MODE_STATIC_GRADIENT:
-            oled_write_P(PSTR("Gradient\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_STATIC_GRADIENT), false);
             break;
 #        endif
 #        ifdef RGBLIGHT_EFFECT_RGB_TEST
         case RGBLIGHT_MODE_RGB_TEST:
-            oled_write_P(PSTR("RGB test\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_RGB_TEST), false);
             break;
 #        endif
 #        ifdef RGBLIGHT_EFFECT_ALTERNATING
         case RGBLIGHT_MODE_ALTERNATING:
-            oled_write_P(PSTR("Alternating\n"), false);
+            oled_write_P(PSTR(OLED_TEXT_RGBLIGHT_MODE_ALTERNATING), false);
             break;
 #        endif
         default:
             break;
     }
+    oled_advance_page(true);
 }
 #    endif  // RGBLIGHT_ENABLE
 
@@ -141,187 +162,187 @@ extern rgb_config_t rgb_matrix_config;
 const char* rgb_matrix_effect_name(uint8_t mode) {
     switch (mode) {
         case RGB_MATRIX_NONE:
-            return PSTR("None");
+            return PSTR(OLED_TEXT_RGB_MATRIX_NONE);
         case RGB_MATRIX_SOLID_COLOR:
-            return PSTR("Solid color");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_COLOR);
 #        ifndef DISABLE_RGB_MATRIX_ALPHAS_MODS
         case RGB_MATRIX_ALPHAS_MODS:
-            return PSTR("Alpha mods");
+            return PSTR(OLED_TEXT_RGB_MATRIX_ALPHAS_MODS);
 #        endif  // DISABLE_RGB_MATRIX_ALPHAS_MODS
 #        ifndef DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
         case RGB_MATRIX_GRADIENT_UP_DOWN:
-            return PSTR("Gradient up/down");
+            return PSTR(OLED_TEXT_RGB_MATRIX_GRADIENT_UP_DOWN);
 #        endif  // DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
 #        ifndef DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
         case RGB_MATRIX_GRADIENT_LEFT_RIGHT:
-            return PSTR("Gradient l/r");
+            return PSTR(OLED_TEXT_RGB_MATRIX_GRADIENT_LEFT_RIGHT);
 #        endif  // DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
 #        ifndef DISABLE_RGB_MATRIX_BREATHING
         case RGB_MATRIX_BREATHING:
-            return PSTR("Breathing");
+            return PSTR(OLED_TEXT_RGB_MATRIX_BREATHING);
 #        endif  // DISABLE_RGB_MATRIX_BREATHING
 #        ifndef DISABLE_RGB_MATRIX_BAND_SAT
         case RGB_MATRIX_BAND_SAT:
-            return PSTR("Band Sat");
+            return PSTR(OLED_TEXT_RGB_MATRIX_BAND_SAT);
 #        endif  // DISABLE_RGB_MATRIX_BAND_SAT
 #        ifndef DISABLE_RGB_MATRIX_BAND_VAL
         case RGB_MATRIX_BAND_VAL:
-            return PSTR("Band Val");
+            return PSTR(OLED_TEXT_RGB_MATRIX_BAND_VAL);
 #        endif  // DISABLE_RGB_MATRIX_BAND_VAL
 #        ifndef DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
         case RGB_MATRIX_BAND_PINWHEEL_SAT:
-            return PSTR("Band PW Sat");
+            return PSTR(OLED_TEXT_RGB_MATRIX_BAND_PINWHEEL_SAT);
 #        endif  // DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
 #        ifndef DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
         case RGB_MATRIX_BAND_PINWHEEL_VAL:
-            return PSTR("Band PW Val");
+            return PSTR(OLED_TEXT_RGB_MATRIX_BAND_PINWHEEL_VAL);
 #        endif  // DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
 #        ifndef DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
         case RGB_MATRIX_BAND_SPIRAL_SAT:
-            return PSTR("Band Spiral Sat");
+            return PSTR(OLED_TEXT_RGB_MATRIX_BAND_SPIRAL_SAT);
 #        endif  // DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
 #        ifndef DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
         case RGB_MATRIX_BAND_SPIRAL_VAL:
-            return PSTR("Band Spiral Val");
+            return PSTR(OLED_TEXT_RGB_MATRIX_BAND_SPIRAL_VAL);
 #        endif  // DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
 #        ifndef DISABLE_RGB_MATRIX_CYCLE_ALL
         case RGB_MATRIX_CYCLE_ALL:
-            return PSTR("Cycle all");
+            return PSTR(OLED_TEXT_RGB_MATRIX_CYCLE_ALL);
 #        endif  // DISABLE_RGB_MATRIX_CYCLE_ALL
 #        ifndef DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
         case RGB_MATRIX_CYCLE_LEFT_RIGHT:
-            return PSTR("Cycle left/right");
+            return PSTR(OLED_TEXT_RGB_MATRIX_CYCLE_LEFT_RIGHT);
 #        endif  // DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
 #        ifndef DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
         case RGB_MATRIX_CYCLE_UP_DOWN:
-            return PSTR("Cycle up/down");
+            return PSTR(OLED_TEXT_RGB_MATRIX_CYCLE_UP_DOWN);
 #        endif  // DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
 #        ifndef DISABLE_RGB_MATRIX_CYCLE_OUT_IN
         case RGB_MATRIX_CYCLE_OUT_IN:
-            return PSTR("Cycle Out/In");
+            return PSTR(OLED_TEXT_RGB_MATRIX_CYCLE_OUT_IN);
 #        endif  // DISABLE_RGB_MATRIX_CYCLE_OUT_IN
 #        ifndef DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
         case RGB_MATRIX_CYCLE_OUT_IN_DUAL:
-            return PSTR("Cycle Out/In Dual");
+            return PSTR(OLED_TEXT_RGB_MATRIX_CYCLE_OUT_IN_DUAL);
 #        endif  // DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
 #        ifndef DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
         case RGB_MATRIX_RAINBOW_MOVING_CHEVRON:
-            return PSTR("Moving chevron");
+            return PSTR(OLED_TEXT_RGB_MATRIX_RAINBOW_MOVING_CHEVRON);
 #        endif  // DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
 #        ifndef DISABLE_RGB_MATRIX_DUAL_BEACON
         case RGB_MATRIX_DUAL_BEACON:
-            return PSTR("Dual beacon");
+            return PSTR(OLED_TEXT_RGB_MATRIX_DUAL_BEACON);
 #        endif  // DISABLE_RGB_MATRIX_DUAL_BEACON
 #        ifndef DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
         case RGB_MATRIX_CYCLE_PINWHEEL:
-            return PSTR("Cycle Pinwheel");
+            return PSTR(OLED_TEXT_RGB_MATRIX_CYCLE_PINWHEEL);
 #        endif  // DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
 #        ifndef DISABLE_RGB_MATRIX_CYCLE_SPIRAL
         case RGB_MATRIX_CYCLE_SPIRAL:
-            return PSTR("Cycle spiral");
+            return PSTR(OLED_TEXT_RGB_MATRIX_CYCLE_SPIRAL);
 #        endif  // DISABLE_RGB_MATRIX_CYCLE_SPIRAL
 #        ifndef DISABLE_RGB_MATRIX_RAINBOW_BEACON
         case RGB_MATRIX_RAINBOW_BEACON:
-            return PSTR("Rainbow beacon");
+            return PSTR(OLED_TEXT_RGB_MATRIX_RAINBOW_BEACON);
 #        endif  // DISABLE_RGB_MATRIX_RAINBOW_BEACON
 #        ifndef DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
         case RGB_MATRIX_PINWHEELS:
-            return PSTR("Pinwheels");
+            return PSTR(OLED_TEXT_RGB_MATRIX_PINWHEELS);
 #        endif  // DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
 #        ifndef DISABLE_RGB_MATRIX_RAINDROPS
         case RGB_MATRIX_RAINDROPS:
-            return PSTR("Raindrops");
+            return PSTR(OLED_TEXT_RGB_MATRIX_RAINDROPS);
 #        endif  // DISABLE_RGB_MATRIX_RAINDROPS
 #        ifndef DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
         case RGB_MATRIX_JELLYBEAN_RAINDROPS:
-            return PSTR("Jellybean rain");
+            return PSTR(OLED_TEXT_RGB_MATRIX_JELLYBEAN_RAINDROPS);
 #        endif  // DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
 #        ifdef RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #            ifndef DISABLE_RGB_MATRIX_TYPING_HEATMAP
         case RGB_MATRIX_TYPING_HEATMAP:
-            return PSTR("Typing heatmap");
+            return PSTR(OLED_TEXT_RGB_MATRIX_TYPING_HEATMAP);
 #            endif  // DISABLE_RGB_MATRIX_TYPING_HEATMAP
 #            ifndef DISABLE_RGB_MATRIX_DIGITAL_RAIN
         case RGB_MATRIX_DIGITAL_RAIN:
-            return PSTR("Digital rain");
+            return PSTR(OLED_TEXT_RGB_MATRIX_DIGITAL_RAIN);
 #            endif  // DISABLE_RGB_MATRIX_DIGITAL_RAIN
-#        endif      // RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#        endif  // RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #        ifdef RGB_MATRIX_KEYREACTIVE_ENABLED
 #            ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE
         case RGB_MATRIX_SOLID_REACTIVE:
-            return PSTR("Solid reactive");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_REACTIVE
 #            ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
         case RGB_MATRIX_SOLID_REACTIVE_SIMPLE:
-            return PSTR("Reactive simple");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_SIMPLE);
 #            endif
 #            ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
         case RGB_MATRIX_SOLID_REACTIVE_WIDE:
-            return PSTR("Reactive wide");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_WIDE);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
 #            ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
         case RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE:
-            return PSTR("Reactive wide");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
 #            ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
         case RGB_MATRIX_SOLID_REACTIVE_CROSS:
-            return PSTR("Reactive cross");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_CROSS);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
 #            ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
         case RGB_MATRIX_SOLID_REACTIVE_MULTICROSS:
-            return PSTR("Reactive cross");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
 #            ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
         case RGB_MATRIX_SOLID_REACTIVE_NEXUS:
-            return PSTR("Reactive nexus");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_NEXUS);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
 #            ifndef DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
         case RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS:
-            return PSTR("Reactive nexus");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
 #            ifndef DISABLE_RGB_MATRIX_SPLASH
         case RGB_MATRIX_SPLASH:
-            return PSTR("Splash");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SPLASH);
 #            endif  // DISABLE_RGB_MATRIX_SPLASH
 #            ifndef DISABLE_RGB_MATRIX_MULTISPLASH
         case RGB_MATRIX_MULTISPLASH:
-            return PSTR("Splash");
+            return PSTR(OLED_TEXT_RGB_MATRIX_MULTISPLASH);
 #            endif  // DISABLE_RGB_MATRIX_MULTISPLASH
 #            ifndef DISABLE_RGB_MATRIX_SOLID_SPLASH
         case RGB_MATRIX_SOLID_SPLASH:
-            return PSTR("Solid splash");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_SPLASH);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_SPLASH
 #            ifndef DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
         case RGB_MATRIX_SOLID_MULTISPLASH:
-            return PSTR("Solid splash");
+            return PSTR(OLED_TEXT_RGB_MATRIX_SOLID_MULTISPLASH);
 #            endif  // DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
-#        endif      // RGB_MATRIX_KEYREACTIVE_ENABLED
+#        endif  // RGB_MATRIX_KEYREACTIVE_ENABLED
 #        if defined(RGB_MATRIX_CUSTOM_USER) && defined(FLAVOR_DASHER)
         case RGB_MATRIX_CUSTOM_DASHER:
-            return PSTR("Dasher & Dancer");
+            return PSTR(OLED_TEXT_FLAVOR_DASHER);
 #        endif  // FLAVOR_DASHER
 #        if defined(RGB_MATRIX_CUSTOM_USER) && defined(FLAVOR_GODSPEED)
         case RGB_MATRIX_CUSTOM_GODSPEED:
-            return PSTR("Godspeed");
+            return PSTR(OLED_TEXT_FLAVOR_GODSPEED);
 #        endif  // FLAVOR_GODSPEED
 #        if defined(RGB_MATRIX_CUSTOM_USER) && defined(FLAVOR_LEVIATHAN)
         case RGB_MATRIX_CUSTOM_LEVIATHAN:
-            return PSTR("Leviathan");
+            return PSTR(OLED_TEXT_FLAVOR_LEVIATHAN);
 #        endif  // FLAVOR_LEVIATHAN
 #        if defined(RGB_MATRIX_CUSTOM_USER) && defined(FLAVOR_TROUBLED_MINDS)
         case RGB_MATRIX_CUSTOM_TROUBLED_MINDS:
-            return PSTR("Troubled minds");
+            return PSTR(OLED_TEXT_FLAVOR_TROUBLED_MINDS);
 #        endif  // FLAVOR_TROUBLED_MINDS
 #        if defined(RGB_MATRIX_CUSTOM_USER) && defined(FLAVOR_MILKSHAKE)
         case RGB_MATRIX_CUSTOM_MILKSHAKE:
-            return PSTR("Milkshake");
+            return PSTR(OLED_TEXT_FLAVOR_MILKSHAKE);
 #        endif  // FLAVOR_MILKSHAKE
 #        if defined(RGB_MATRIX_CUSTOM_USER) && defined(FLAVOR_SERIKA)
         case RGB_MATRIX_CUSTOM_SERIKA:
-            return PSTR("Serika");
+            return PSTR(OLED_TEXT_FLAVOR_SERIKA);
 #        endif  // FLAVOR_SERIKA
         default:
-            return PSTR("unknown");
+            return PSTR(OLED_TEXT_UNKNOWN);
     }
 }
 
--- users/buz/oled_stuff.h
+++ users/buz/oled_stuff.h
@@ -1,6 +1,201 @@
 #pragma once
 #include "quantum.h"
 
+/*
+ * TODO: I need a way to distinguish different types of display layouts.
+ *
+ * - 128x64
+ * - 128x32
+ * - 32x128
+ */
+
+#if defined(OLED_DISPLAY_128X64)  // Kyria
+#    define OLED_TEXT_UNKNOWN "Unknown"
+#    define OLED_TEXT_NONE "None"
+
+// Default layouts
+#    define OLED_TEXT_LAYOUT_NAME "Layer: "
+#    define OLED_TEXT_LAYOUT_QUERTY "QUERTY"
+#    define OLED_TEXT_LAYOUT_COLEMAK "Colemak"
+
+// Layer names
+#    define OLED_TEXT_LAYER_LOWER "Lower"
+#    define OLED_TEXT_LAYER_RAISE "Raise"
+#    define OLED_TEXT_LAYER_ADJUST "Adjust"
+#    define OLED_TEXT_LAYER_VIM "Vim"
+#    define OLED_TEXT_LAYER_NUM "Numpad"
+#    define OLED_TEXT_LAYER_MOUSE "Mouse"
+#    define OLED_TEXT_LAYER_FN "FN"
+#    define OLED_TEXT_LAYER_MEDIA "Media"
+
+// Mod names
+#    define OLED_TEXT_MOD_NAME "Mods: "
+#    define OLED_TEXT_MOD_CTRL "CTRL"
+#    define OLED_TEXT_MOD_GUI "GUI"
+#    define OLED_TEXT_MOD_ALT "ALT"
+#    define OLED_TEXT_MOD_SHIFT "SFT"
+#    define OLED_TEXT_MOD_CAPS "CAP"
+
+// Leader mode
+#    define OLED_TEXT_LEADING "(lead)"
+
+// RGB underglow effects
+#    define OLED_TEXT_RGBLIGHT_MODE_STATIC_LIGHT "Static"
+#    define OLED_TEXT_RGBLIGHT_MODE_BREATHING "Breathing"
+#    define OLED_TEXT_RGBLIGHT_MODE_RAINBOW_MOOD "Rainbow mood"
+#    define OLED_TEXT_RGBLIGHT_MODE_RAINBOW_SWIRL "Rainbow swirl"
+#    define OLED_TEXT_RGBLIGHT_MODE_SNAKE "Snake"
+#    define OLED_TEXT_RGBLIGHT_MODE_KNIGHT "Knight"
+#    define OLED_TEXT_RGBLIGHT_MODE_CHRISTMAS "Christmas"
+#    define OLED_TEXT_RGBLIGHT_MODE_STATIC_GRADIENT "Gradient"
+#    define OLED_TEXT_RGBLIGHT_MODE_RGB_TEST "RGB Test"
+#    define OLED_TEXT_RGBLIGHT_MODE_ALTERNATING "Alternating"
+
+// RGB matrix effects
+#    define OLED_TEXT_RGB_MATRIX_NONE "None"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_COLOR "Solid Color"
+#    define OLED_TEXT_RGB_MATRIX_ALPHAS_MODS "Alpha Mods"
+#    define OLED_TEXT_RGB_MATRIX_GRADIENT_UP_DOWN "Gradient u/d"
+#    define OLED_TEXT_RGB_MATRIX_GRADIENT_LEFT_RIGHT "Gradient l/r"
+#    define OLED_TEXT_RGB_MATRIX_BREATHING "Breating"
+#    define OLED_TEXT_RGB_MATRIX_BAND_SAT "Band Sat"
+#    define OLED_TEXT_RGB_MATRIX_BAND_VAL "Band Val"
+#    define OLED_TEXT_RGB_MATRIX_BAND_PINWHEEL_SAT "PW Sat"
+#    define OLED_TEXT_RGB_MATRIX_BAND_PINWHEEL_VAL "PW Val"
+#    define OLED_TEXT_RGB_MATRIX_BAND_SPIRAL_SAT "Spiral Sat"
+#    define OLED_TEXT_RGB_MATRIX_BAND_SPIRAL_VAL "Spiral Val"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_ALL "Cycle All"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_LEFT_RIGHT "Cycle l/r"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_UP_DOWN "Cycle u/d"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_OUT_IN "Cycle i/o"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_OUT_IN_DUAL "Cycle dual"
+#    define OLED_TEXT_RGB_MATRIX_RAINBOW_MOVING_CHEVRON "Moving Chevron"
+#    define OLED_TEXT_RGB_MATRIX_DUAL_BEACON "Dual Beacon"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_PINWHEEL "Cycle Pinwheel"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_SPIRAL "Cycle Spiral"
+#    define OLED_TEXT_RGB_MATRIX_RAINBOW_BEACON "Rainbow Beacon"
+#    define OLED_TEXT_RGB_MATRIX_PINWHEELS "Pinwheels"
+#    define OLED_TEXT_RGB_MATRIX_RAINDROPS "Raindrops"
+#    define OLED_TEXT_RGB_MATRIX_JELLYBEAN_RAINDROPS "Jellybean"
+#    define OLED_TEXT_RGB_MATRIX_TYPING_HEATMAP "Heatmap"
+#    define OLED_TEXT_RGB_MATRIX_DIGITAL_RAIN "Rain"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE "Solic reactive"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_SIMPLE "Reactive simple"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_WIDE "Reactive wide"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE "Reactive wide"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_CROSS "Reactive Cross"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS "Reactive Cross"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_NEXUS "Reactive Nexus"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS "Reactive Nexus"
+#    define OLED_TEXT_RGB_MATRIX_SPLASH "Splash"
+#    define OLED_TEXT_RGB_MATRIX_MULTISPLASH "Splash"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_SPLASH "Solid Splash"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_MULTISPLASH "Solid Splash"
+
+// Custom flavor names
+#    define OLED_TEXT_FLAVOR_DASHER "Dasher & Dancer"
+#    define OLED_TEXT_FLAVOR_GODSPEED "Godspeed"
+#    define OLED_TEXT_FLAVOR_LEVIATHAN "Leviathan"
+#    define OLED_TEXT_FLAVOR_TROUBLED_MINDS "Troubled Minds"
+#    define OLED_TEXT_FLAVOR_MILKSHAKE "Milkshake"
+#    define OLED_TEXT_FLAVOR_SERIKA "Serika"
+
+#elif defined(OLED_DISPLAY_VERTICAL)  // Experimental vertical mode (Crkbd)
+#    define OLED_TEXT_UNKNOWN "?"
+#    define OLED_TEXT_NONE "X"
+
+#else  // crkbd, lily58, helix
+#    define OLED_TEXT_UNKNOWN "Unknown"
+#    define OLED_TEXT_NONE "None"
+
+// Default layouts
+#    define OLED_TEXT_LAYOUT_NAME "L: "
+#    define OLED_TEXT_LAYOUT_QUERTY "QUERTY"
+#    define OLED_TEXT_LAYOUT_COLEMAK "Colemak"
+
+// Layer names
+#    define OLED_TEXT_LAYER_LOWER "Lower"
+#    define OLED_TEXT_LAYER_RAISE "Raise"
+#    define OLED_TEXT_LAYER_ADJUST "Adj"
+#    define OLED_TEXT_LAYER_VIM "Vim"
+#    define OLED_TEXT_LAYER_NUM "Num"
+#    define OLED_TEXT_LAYER_MOUSE "Mouse"
+#    define OLED_TEXT_LAYER_FN "FN"
+#    define OLED_TEXT_LAYER_MEDIA "Media"
+
+// Mod names
+#    define OLED_TEXT_MOD_NAME "M: "
+#    define OLED_TEXT_MOD_CTRL "CTRL"
+#    define OLED_TEXT_MOD_GUI "GUI"
+#    define OLED_TEXT_MOD_ALT "ALT"
+#    define OLED_TEXT_MOD_SHIFT "SFT"
+#    define OLED_TEXT_MOD_CAPS "CAP"
+
+// Leader mode
+#    define OLED_TEXT_LEADING "(lead)"
+
+// RGB underglow effects
+#    define OLED_TEXT_RGBLIGHT_MODE_STATIC_LIGHT "Static"
+#    define OLED_TEXT_RGBLIGHT_MODE_BREATHING "Breathing"
+#    define OLED_TEXT_RGBLIGHT_MODE_RAINBOW_MOOD "Rainbow mood"
+#    define OLED_TEXT_RGBLIGHT_MODE_RAINBOW_SWIRL "Rainbow swirl"
+#    define OLED_TEXT_RGBLIGHT_MODE_SNAKE "Snake"
+#    define OLED_TEXT_RGBLIGHT_MODE_KNIGHT "Knight"
+#    define OLED_TEXT_RGBLIGHT_MODE_CHRISTMAS "Christmas"
+#    define OLED_TEXT_RGBLIGHT_MODE_STATIC_GRADIENT "Gradient"
+#    define OLED_TEXT_RGBLIGHT_MODE_RGB_TEST "RGB Test"
+#    define OLED_TEXT_RGBLIGHT_MODE_ALTERNATING "Alternating"
+
+// RGB matrix effects
+#    define OLED_TEXT_RGB_MATRIX_NONE "None"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_COLOR "Solid Color"
+#    define OLED_TEXT_RGB_MATRIX_ALPHAS_MODS "Alpha Mods"
+#    define OLED_TEXT_RGB_MATRIX_GRADIENT_UP_DOWN "Gradient u/d"
+#    define OLED_TEXT_RGB_MATRIX_GRADIENT_LEFT_RIGHT "Gradient l/r"
+#    define OLED_TEXT_RGB_MATRIX_BREATHING "Breating"
+#    define OLED_TEXT_RGB_MATRIX_BAND_SAT "Band Sat"
+#    define OLED_TEXT_RGB_MATRIX_BAND_VAL "Band Val"
+#    define OLED_TEXT_RGB_MATRIX_BAND_PINWHEEL_SAT "PW Sat"
+#    define OLED_TEXT_RGB_MATRIX_BAND_PINWHEEL_VAL "PW Val"
+#    define OLED_TEXT_RGB_MATRIX_BAND_SPIRAL_SAT "Spiral Sat"
+#    define OLED_TEXT_RGB_MATRIX_BAND_SPIRAL_VAL "Spiral Val"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_ALL "Cycle All"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_LEFT_RIGHT "Cycle l/r"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_UP_DOWN "Cycle u/d"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_OUT_IN "Cycle i/o"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_OUT_IN_DUAL "Cycle dual"
+#    define OLED_TEXT_RGB_MATRIX_RAINBOW_MOVING_CHEVRON "Moving Chevron"
+#    define OLED_TEXT_RGB_MATRIX_DUAL_BEACON "Dual Beacon"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_PINWHEEL "Cycle Pinwheel"
+#    define OLED_TEXT_RGB_MATRIX_CYCLE_SPIRAL "Cycle Spiral"
+#    define OLED_TEXT_RGB_MATRIX_RAINBOW_BEACON "Rainbow Beacon"
+#    define OLED_TEXT_RGB_MATRIX_PINWHEELS "Pinwheels"
+#    define OLED_TEXT_RGB_MATRIX_RAINDROPS "Raindrops"
+#    define OLED_TEXT_RGB_MATRIX_JELLYBEAN_RAINDROPS "Jellybean"
+#    define OLED_TEXT_RGB_MATRIX_TYPING_HEATMAP "Heatmap"
+#    define OLED_TEXT_RGB_MATRIX_DIGITAL_RAIN "Rain"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE "Solic reactive"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_SIMPLE "Reactive simple"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_WIDE "Reactive wide"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE "Reactive wide"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_CROSS "Reactive Cross"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS "Reactive Cross"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_NEXUS "Reactive Nexus"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS "Reactive Nexus"
+#    define OLED_TEXT_RGB_MATRIX_SPLASH "Splash"
+#    define OLED_TEXT_RGB_MATRIX_MULTISPLASH "Splash"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_SPLASH "Solid Splash"
+#    define OLED_TEXT_RGB_MATRIX_SOLID_MULTISPLASH "Solid Splash"
+
+// Custom flavor names
+#    define OLED_TEXT_FLAVOR_DASHER "Dasher & Dancer"
+#    define OLED_TEXT_FLAVOR_GODSPEED "Godspeed"
+#    define OLED_TEXT_FLAVOR_LEVIATHAN "Leviathan"
+#    define OLED_TEXT_FLAVOR_TROUBLED_MINDS "Troubled Minds"
+#    define OLED_TEXT_FLAVOR_MILKSHAKE "Milkshake"
+#    define OLED_TEXT_FLAVOR_SERIKA "Serika"
+#endif
+
 void oled_render_layer(void);
 void oled_render_mods(void);
 void oled_render_rgb_matrix_effect_name(void);
