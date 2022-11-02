5ebc98fa12d60afd891bd9d1cf31135ea175c0ca
Bastian Winkler <buz@serious.im>
2022-11-02 11:03:16 +0100

    Revert "Add workaround for combo layer detection"
    This reverts commit 3f33619f57cff6706ad94d90aae25556e4c74097.

--- users/buz/combos.c
+++ users/buz/combos.c
@@ -1,7 +1,5 @@
 #include "buz.h"
 
-bool no_combo_layer;
-
 enum combos {
     WE_ESC,
     SD_TAB,
@@ -9,6 +7,7 @@ enum combos {
     KL_BSPC,
     COMBO_IE,
     COMBO_XC,
+    COMBO_CODOT,
     COMBO_FJ,
     COMBO_LENGTH,
 };
@@ -21,6 +20,7 @@ const uint16_t PROGMEM sd_combo[] = {LALT_T(KC_S), LCTL_T(KC_D), COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {RCTL_T(KC_K), LALT_T(KC_L), COMBO_END};
 const uint16_t PROGMEM ie_combo[] = {KC_I, KC_E, COMBO_END};
 const uint16_t PROGMEM xc_combo[] = {KC_X, LT(_TMUX, KC_C), COMBO_END};
+const uint16_t PROGMEM codot_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM fj_combo[] = {LSFT_T(KC_F), RSFT_T(KC_J), COMBO_END};
 
 // clang-format off
@@ -31,10 +31,11 @@ combo_t key_combos[] = {
     [KL_BSPC]     = COMBO(kl_combo, KC_BSPC),
     [COMBO_IE]    = COMBO(ie_combo, IFERR),
     [COMBO_XC]    = COMBO(xc_combo, LCTL(KC_SPACE)),
+    [COMBO_CODOT] = COMBO(codot_combo, RCTL(KC_SPACE)),
     [COMBO_FJ]    = COMBO(fj_combo, CAPS_WORD),
 };
 
 // disable combos on some layers, especially symbol pairs like () are sometimes typed too fast.
 bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
-  return !no_combo_layer;
+  return layer_state_is(_QWERTY);
 }
